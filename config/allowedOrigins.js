c1d484c3b68a736907c78f0e0a4f63d0d894c8eb
Parivallal <johnparijv@gmail.com>
2024-12-22 01:08:52 +0530

    reset password added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:3500", "http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:3500",
+  "http://localhost:3000",
+  "https://taskgenieserver-44u2.onrender.com",
+];
 
 module.exports = allowedOrigins;
