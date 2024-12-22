15bf41a53dd67a629d860a831bf91f2c85263dbe
Parivallal <johnparijv@gmail.com>
2024-12-22 08:05:37 +0530

    cors err fix

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3500",
   "http://localhost:3000",
   "https://taskgenieserver-44u2.onrender.com",
+  "https://taskygeniee.netlify.app",
 ];
 
 module.exports = allowedOrigins;
